fc34dbc44f15e29e91ccbb2f7f93ad68faf39006
Fumio Nonaka <fumiononaka@FumioNonaka.local>
2020-08-12 01:24:11 +0900

    react-simplebar-01

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header id="header" className="text-white bg-primary w-100 p-2 d-flex">
+      <h1>Header</h1>
+    </header>
+  );
+};
+
+export default Header;
