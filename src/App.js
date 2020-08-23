1087f5aee9ae901dd11291bbc4affade1ff4423a
Fumio Nonaka <fumiononaka@FumioNonaka.local>
2020-08-24 00:37:29 +0900

    react-simplebar-04

--- src/App.js
+++ src/App.js
@@ -7,24 +7,31 @@ import MainContents from "./components/MainContents";
 import LeftColumn from "./components/LeftColumn";
 
 function App() {
+  const [leftColumnWidth, setLeftColumnWidth] = useState(0);
   const [headerHeight, setHeaderHeight] = useState(0);
   const [leftColumnHeight, setLeftColumnHeight] = useState(0);
   const header = useRef(null);
+  const leftColumn = useRef(null);
   useEffect(() => {
     const _header = header.current;
+    const _leftColumn = leftColumn.current;
     const setLayout = () => {
       setHeaderHeight(_header.clientHeight);
+      setLeftColumnWidth(_leftColumn.clientWidth);
       setLeftColumnHeight(window.innerHeight - _header.clientHeight);
     };
     window.addEventListener("resize", setLayout);
     setLayout();
-  }, []);
+  }, [leftColumn]);
   return (
     <div className="App" style={{ paddingTop: headerHeight }}>
       <Header headerRef={header} />
       <div className="container-fluid d-flex px-0">
-        <LeftColumn leftColumnHeight={leftColumnHeight} />
-        <MainContents />
+        <LeftColumn
+          leftColumnRef={leftColumn}
+          leftColumnHeight={leftColumnHeight}
+        />
+        <MainContents leftColumnWidth={leftColumnWidth} />
       </div>
     </div>
   );
--- src/components/LeftColumn.js
+++ src/components/LeftColumn.js
@@ -1,26 +1,30 @@
 import React from "react";
 import SimpleBar from "simplebar-react";
 
-// const LeftColumn = () => {
-const LeftColumn = ({ leftColumnHeight, leftColumnRef }) => {
+const LeftColumn = ({ headerHeight, leftColumnHeight, leftColumnRef }) => {
   return (
-    // <div id="left-column" className="bg-light p-2">
-    <SimpleBar
+    <div
       id="left-column"
       ref={leftColumnRef}
       className="bg-light p-2"
       style={{
-        height: leftColumnHeight,
+        position: "fixed",
+        top: headerHeight,
       }}
     >
-      <h3>Left column</h3>
-      <ul id="list" className="pl-4">
-        {Array.from(new Array(20), (_, index) => (
-          <li key={index}>item {String(index + 1).padStart(2, 0)}</li>
-        ))}
-      </ul>
-      {/* </div> */}
-    </SimpleBar>
+      <SimpleBar
+        style={{
+          height: leftColumnHeight,
+        }}
+      >
+        <h3>Left column</h3>
+        <ul id="list" className="pl-4">
+          {Array.from(new Array(20), (_, index) => (
+            <li key={index}>item {String(index + 1).padStart(2, 0)}</li>
+          ))}
+        </ul>
+      </SimpleBar>
+    </div>
   );
 };
 
